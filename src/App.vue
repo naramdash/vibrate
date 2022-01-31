07e771d463e4b357ae0fab97ce61c0537e8ecfa6
kimjuho_daim <kimjuho@daimresearch.com>
2022-01-31 17:33:23 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue'
+import { useVibrate } from '@vueuse/core'
+
+const { vibrate, stop, isSupported } = useVibrate()
+
+</script>
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+
+  <h1 @mousedown="vibrate()" @mouseup="stop()">Vibe Me</h1>
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
