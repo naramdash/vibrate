2b7d7bc3a4f401feebcfe09b3dceeecc46e66ab4
kimjuho_daim <kimjuho@daimresearch.com>
2022-01-31 17:44:12 +0900

    touchevent and support check

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,32 @@
 import HelloWorld from './components/HelloWorld.vue'
 import { useVibrate } from '@vueuse/core'
 
-const { vibrate, stop, isSupported } = useVibrate()
+const { vibrate, stop, isSupported } = useVibrate({ pattern: [300, 100, 300] })
+
+function startVibrate() {
+  if (isSupported)
+    vibrate()
+  else
+    alert("vibrate not supported")
+}
+
+function endVibrate() {
+  stop()
+}
 
 </script>
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
 
-  <h1 @mousedown="vibrate()" @mouseup="stop()">Vibe Me</h1>
+  <h1
+    @mousedown="startVibrate()"
+    @mousemove="startVibrate()"
+    @mouseup="endVibrate()"
+    @touchstart="startVibrate()"
+    @touchmove="startVibrate()"
+    @touchend="endVibrate()"
+  >Vibe Me</h1>
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
 
